762ab5684349cb418e0649c0903798ec34e2fded
sinius15 <sinius15@gmail.com>
2013-11-22 13:48:06 +0100

    the graphical is turned off gives a better average time

--- src/com/sinius/ordener/Game.java
+++ src/com/sinius/ordener/Game.java
@@ -3,7 +3,6 @@ package com.sinius.ordener;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-import com.sinius.ordener.gui.Display;
 import com.sinius.ordener.gui.Results;
 import com.sinius.ordener.sorters.Sorter;
 
@@ -12,7 +11,6 @@ public class Game {
 	private static Game thiss;
 	public int[] toDraw;
 	public int highest;
-	private Display frame;
 	public boolean running = true;
 	public String time;
 	public String status = ""; 
@@ -20,29 +18,19 @@ public class Game {
 	private String[] rsltS;
 	private int[] rsltI;
 	
-	public Game(final Sorter alg, final int[] toSort, final int runs){
+	public Game(final Sorter alg, final int length, final int runs){
 		thiss = this;
-		toDraw = toSort;
-		highest = Util.getHighest(toSort);
-		frame = new Display(toSort.length*10+toSort.length+2, highest * 5, "sorter", Color.black);
+		
+//		frame = new Display(toSort.length*10+toSort.length+2, highest * 5, "sorter", Color.black);
 		rsltS = new String[runs];
 		rsltI = new int[runs];
 		
 		new Thread(new Runnable() {@Override public void run() {
-			status = "Start in 3...";
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {}
-			status = "Start in 2...";
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {}
-			status = "Start in 1...";
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {}
-			status = "";
 			for(int i = 0 ; i < runs; i++){
+				int[] toSort = Util.randomIntArr(length);
+				toDraw = toSort;
+				highest = Util.getHighest(toSort);
+
 				TimeTimer t = new TimeTimer();
 				t.Start();
 				toDraw = alg.sort(toSort);
@@ -56,14 +44,14 @@ public class Game {
 			
 		}}).start();;
 		
-		new Thread(new Runnable() {@Override public void run() {
-			while(running){
-				frame.reDraw();
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {}
-			}
-		}}).start();
+//		new Thread(new Runnable() {@Override public void run() {
+//			while(running){
+//				frame.reDraw();
+//				try {
+//					Thread.sleep(100);
+//				} catch (InterruptedException e) {}
+//			}
+//		}}).start();
 	}
 	
 	public void reDraw(Graphics2D g){
